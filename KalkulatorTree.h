5485cca2a46d8757dbcbc27d8956a7d603cffab0
fioraaaa <fiora.berliana.tif420@polban.ac.id>
2021-08-03 22:35:42 +0700

    revisi

--- KalkulatorTree.h
+++ KalkulatorTree.h
@@ -10,27 +10,16 @@
 #ifndef KALKULATOR_H
 #define KALKULATOR_H
 
-#define Nil NULL
-#define Info(P) (P)->info
-#define Left(P) (P)->left
-#define Right(P) (P)->right
-#define Next(P) (P)->next
-
 #include "StackForTree.h"
 #include "BinaryTree.h"
 #include "BangunDatar.h"
 #include <math.h>
 
-typedef char String[50];
-typedef char infotypeTree[10];
-typedef struct treeNode *addressTree;
-typedef addressTree Kalkulator;
-
 /* Membuat sebuah ekspresi tree dari ekspresi postfix yang sudah didapatkan. 
  * I.S. : Postfix terdefinisi.
  * F.S. : Mengembalikan ekspresi tree.
  */
-Kalkulator BuildExpressionTree(infotypeTree postfix);
+BinTree BuildExpressionTree(infotypeTree postfix);
 
 /* Mengkonversi ekspresi infix menjadi ekspresi postfix.
  * I.S. : Infix terdefinisi.
@@ -42,7 +31,7 @@ void InfixToPostfix(String infix, String postfix);
  * I.S. : P terdefinisi.
  * F.S. : Hasil kalkulasi dari P dikembalikan.
  */
-float CalculationOfTree(Kalkulator P);
+float CalculationOfTree(BinTree P);
 
 /* Menampilkan menu untuk kalkulator.
  * I.S. : Sembarang.
